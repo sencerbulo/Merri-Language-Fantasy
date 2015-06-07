dddbdeffe599b48eeeb76db2d6af18e136a7ea6e
Rachel J Morris <racheljmorris@gmail.com>
2015-06-06 23:53:29 -0500

    Removed Gideros files

--- source/Fantazio/TitleState.lua
+++
@@ -1,2 +0,0 @@
-TitleState = gideros.class( IState )
-
--- source/Fantazio/main.lua
+++
@@ -1,9 +0,0 @@
--- Fantazio de Esperanto / Rachel J Morris / Moosader.com
-
-StateManager:init()
-StateManager:PushState( "title", TitleState.new( { title = "title" } ) )
-
---while ( StateManager:IsDone() == false ) do
---	StateManager:Update()
---	StateManager:Draw()
---end
