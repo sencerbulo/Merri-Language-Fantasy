6e25326ad598cbd7814783b1c00c968b45959bc2
Rachel J Morris <racheljmorris@gmail.com>
2015-08-15 15:16:33 -0500

    New map generation with taller dimensions, added ceiling insted of just walls

--- Merri Language Fantasy/Merri/Games/Miner/Game_Miner.lua
+++ Merri Language Fantasy/Merri/Games/Miner/Game_Miner.lua
@@ -10,24 +10,123 @@ function Map:init( options )
 			ceiling = Texture.new( "Content/Games/Miner/Tiles/ground_darkstone.png" ),
 			
 			rock = Texture.new( "Content/Games/Miner/Tiles/rock.png" ),
-			gemA = Texture.new( "Content/Games/Miner/Tiles/gemA.png" )
-			gemB = Texture.new( "Content/Games/Miner/Tiles/gemB.png" )
-			gemC = Texture.new( "Content/Games/Miner/Tiles/gemC.png" )
-			gemD = Texture.new( "Content/Games/Miner/Tiles/gemD.png" )
-			coinA = Texture.new( "Content/Games/Miner/Tiles/copper_coin.png" )
-			coinB = Texture.new( "Content/Games/Miner/Tiles/silver_coin.png" )
-			coinC = Texture.new( "Content/Games/Miner/Tiles/gold_coin.png" )
-			sandwich = Texture.new( "Content/Games/Miner/Tiles/sandwich.png" )
-			ladder = Texture.new( "Content/Games/Miner/Tiles/ladder_down.png" )
-			snail = Texture.new( "Content/Games/Miner/Characters/enemy_snail.png" )
-			mole = Texture.new( "Content/Games/Miner/Characters/enemy_mole.png" )
+			gemA = Texture.new( "Content/Games/Miner/Tiles/gemA.png" ),
+			gemB = Texture.new( "Content/Games/Miner/Tiles/gemB.png" ),
+			gemC = Texture.new( "Content/Games/Miner/Tiles/gemC.png" ),
+			gemD = Texture.new( "Content/Games/Miner/Tiles/gemD.png" ),
+			coinA = Texture.new( "Content/Games/Miner/Tiles/copper_coin.png" ),
+			coinB = Texture.new( "Content/Games/Miner/Tiles/silver_coin.png" ),
+			coinC = Texture.new( "Content/Games/Miner/Tiles/gold_coin.png" ),
+			sandwich = Texture.new( "Content/Games/Miner/Tiles/sandwich.png" ),
+			ladder = Texture.new( "Content/Games/Miner/Tiles/ladder_down.png" ),
+			snail = Texture.new( "Content/Games/Miner/Characters/enemy_snail.png" ),
+			mole = Texture.new( "Content/Games/Miner/Characters/enemy_mole.png" ),
 		}
 		
-	self.moveAmount = self.textures.ground:getWidth()
+	self.tileWidth = self.textures.ground:getWidth()
+	self.floor = 1
+	self.lastFloor = 20
+	self.mapWidth = 10
+	self.mapHeight = 14
+		
+	self.tiles = {}
+	for x = 0, self.mapWidth do
+		self.tiles[x] = {}
+		for y = 0, self.mapHeight do
+			self.tiles[x][y] = {}
+		end
+	end
 end
 
--- options.floor
-function Map:Generate( options )
+function Map:Generate()
+	-- Initialize entire map as wall
+	for y = 0, self.mapHeight do
+		for x = 0, self.mapWidth do
+			self.tiles[x][y].type = "ceiling"
+			self.tiles[x][y].bitmap = Bitmap.new( self.textures[ self.tiles[x][y].type ] )
+			self.tiles[x][y].bitmap:setPosition( x * self.tileWidth, y * self.tileWidth )
+		end
+	end
+		
+	-- Generate paths
+	local tileCount = math.random( 100, 120 )
+	
+	local startX = 0
+	local startY = math.random( 0, self.mapHeight )
+	local endX = self.mapWidth - 1
+	local endY = math.random( 0, self.mapHeight )
+	
+	-- Traverse Map
+	local x = startX
+	local y = startY
+	
+	local roomPoints = {}
+	table.insert( roomPoints, { x = endX, y = endY } )
+	for i = 0, math.random( 5, 10 ) do
+		local room = { x = math.random( 1, self.mapWidth - 1 ), y = math.random( 1, self.mapHeight - 1 ) }
+		table.insert( roomPoints, room )
+	end
+	
+	-- Add paths between each room
+	for key, point in pairs( roomPoints ) do
+		endX = point.x
+		endY = point.y
+		
+		while ( x ~= endX or y ~= endY ) do
+			self.tiles[x][y].type = "ground"
+			self.tiles[x][y].bitmap:setTexture( self.textures[ self.tiles[x][y].type ] )
+			
+			local xOrY = math.random( 1, 2 )
+			if ( xOrY == 1 ) then
+				if ( endX < x ) then				x = x - 1
+				elseif ( endX > x ) then		x = x + 1
+				end
+			else
+				if ( endY < y ) then				y = y - 1
+				elseif ( endY > y ) then		y = y + 1
+				end
+			end -- if ( xOrY == 1 ) then
+		end -- while ( x ~= endX or y ~= endY ) do
+	end
+	
+	-- last tile
+	self.tiles[x][y].type = "ground"
+	self.tiles[x][y].bitmap:setTexture( self.textures[ self.tiles[x][y].type ] )
+	
+	-- add ceilings: CEILING, WALL, FLOOR
+	for y = 0, self.mapHeight do
+		for x = 0, self.mapWidth do
+			if ( self.tiles[x][y].type == "ground" ) then
+				if ( self.tiles[x][y - 1] ~= nil and self.tiles[x][y - 1].type == "ceiling" 
+					and ( ( self.tiles[x][y - 2] ~= nil and self.tiles[x][y - 2].type == "ceiling" ) or self.tiles[x][y - 2] == nil ) ) then
+				self.tiles[x][y - 1].type = "wall"
+				self.tiles[x][y - 1].bitmap:setTexture( self.textures[ self.tiles[x][y - 1].type ] )
+				
+				elseif ( self.tiles[x][y - 1] ~= nil and self.tiles[x][y - 1].type == "ceiling" and self.tiles[x][y - 2] ~= nil and self.tiles[x][y - 2].type == "ground" ) then
+				self.tiles[x][y - 1].type = "ground"
+				self.tiles[x][y - 1].bitmap:setTexture( self.textures[ self.tiles[x][y - 1].type ] )
+				end
+			end
+		end
+	end
+	
+	-- Add items
+	for y = 0, self.mapHeight do
+		for x = 0, self.mapWidth do
+			if ( self.tiles[x][y].type == "ground" ) then
+			
+			end
+		end
+	end
+	
+end
+
+function Map:Draw()
+	for y = 0, self.mapHeight do
+		for x = 0, self.mapWidth do
+			stage:addChild( self.tiles[x][y].bitmap )			
+		end
+	end
 end
 
 function GameMinerState:init( options )
@@ -52,12 +151,12 @@ function GameMinerState:Setup( options )
 		collect = Sound.new( "Content/Audio/collect.wav" ),
 		footsteps = Sound.new( "Content/Audio/footsteps.wav" ),
 		hurt = Sound.new( "Content/Audio/hurt.wav" ),
-		sword = Sound.new( "Content/Audio/sword.wav" )
+		sword = Sound.new( "Content/Audio/sword.wav" ),
 	}
 	
 	self.fonts = {
 		overhead = TTFont.new( "Content/Fonts/NotoSans-Bold.ttf", 9 ),
-		hud = TTFont.new( "Content/Fonts/NotoSans-Bold.ttf", 14 )
+		hud = TTFont.new( "Content/Fonts/NotoSans-Bold.ttf", 14 ),
 	}
 	
 	-- Fade out graphic
@@ -65,12 +164,20 @@ function GameMinerState:Setup( options )
 	self.transition = false
 	self.fadeBitmap = Bitmap.new( self.textures.black )
 	self.fadeBitmap:setAlpha( 0 )
+	
+	-- Set up map
+	self.map = Map.new()
+	self.map:Generate()
+	
+	self:Draw()
 end
 
 function GameMinerState:Cleanup()
 end
 
 function GameMinerState:Draw()
+	StateBase:Draw()
+	self.map:Draw()
 end
 
 function GameMinerState:SetupMap()
