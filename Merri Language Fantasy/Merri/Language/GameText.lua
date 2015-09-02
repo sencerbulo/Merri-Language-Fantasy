a61ddd0b934baf1f8cd75fb35a9fb062e8c1e449
Rachel J Morris <racheljmorris@gmail.com>
2015-09-01 21:50:54 -0500

    Added Farsi translation from Ali H.

--- Merri Language Fantasy/Merri/Language/GameText.lua
+++ Merri Language Fantasy/Merri/Language/GameText.lua
@@ -2,11 +2,16 @@ GameText  = Core.class()
 
 function GameText:SetupLanguages()
 GameText.LastUpdated = {}
+
+-- Esperanto:       https://en.wikipedia.org/wiki/Esperanto
+-- Ido:             https://en.wikipedia.org/wiki/Ido_%28language%29
+-- Ithkuil:         https://en.wikipedia.org/wiki/Ithkuil
+-- Interlingua:     https://en.wikipedia.org/wiki/Interlingua
     
 GameText.HelperText.English = {}
 GameText.TargetText.English = {}
 GameText.LastUpdated.English = "2015-09-01"
-GameText.HelperText.English   [ "I know English" ]              = "I know English."
+GameText.HelperText.English   [ "I know English" ]              = "I know English"
 GameText.HelperText.English   [ "I am learning English" ]       = "I am learning English"
 GameText.HelperText.English   [ "I am learning Esperanto" ]     = "I am learning Esperanto"
 GameText.HelperText.English   [ "I am learning Ido" ]           = "I am learning Ido"
@@ -19,7 +24,7 @@ GameText.HelperText.English   [ "I am learning Polish" ]        = "I am learning
 GameText.HelperText.Spanish = {}
 GameText.TargetText.Spanish = {}
 GameText.LastUpdated.Spanish = "2015-08-22"
-GameText.HelperText.Spanish[ "I know Spanish" ]                 = "Yo sé Español."
+GameText.HelperText.Spanish[ "I know Spanish" ]                 = "Yo sé Español"
 GameText.HelperText.Spanish[ "I am learning English" ]          = "Estoy aprendiendo Inglés"
 GameText.HelperText.Spanish[ "I am learning Esperanto" ]        = "Estoy aprendiendo Esperanto"
 GameText.HelperText.Spanish[ "I am learning Ido" ]              = "Estoy aprendiendo Ido"
@@ -32,7 +37,7 @@ GameText.HelperText.Spanish[ "I am learning Polish" ]           = ""
 GameText.HelperText.German = {}
 GameText.TargetText.German = {}
 GameText.LastUpdated.German = "2015-08-22"
-GameText.HelperText.German[ "I know German" ]                   = "Ich kann Deutsch."
+GameText.HelperText.German[ "I know German" ]                   = "Ich kann Deutsch"
 GameText.HelperText.German[ "I am learning English" ]           = "Ich lerne Englisch"
 GameText.HelperText.German[ "I am learning Esperanto" ]         = "Ich lerne Esperanto"
 GameText.HelperText.German[ "I am learning Ido" ]               = "Ich lerne Ido"
@@ -45,7 +50,7 @@ GameText.HelperText.German[ "I am learning Polish" ]            = "Ich lerne Pol
 GameText.TargetText.French = {}
 GameText.HelperText.French = {}
 GameText.LastUpdated.French = "2015-09-01"
-GameText.HelperText.French[ "I know French" ]                   = "Je connais le français."
+GameText.HelperText.French[ "I know French" ]                   = "Je connais le français"
 GameText.HelperText.French[ "I am learning English" ]           = "J'apprends l'anglais"
 GameText.HelperText.French[ "I am learning Esperanto" ]         = "J'apprends l'espéranto"
 GameText.HelperText.French[ "I am learning Ido" ]               = "J'apprends l'ido"
@@ -60,54 +65,67 @@ GameText.TargetText.Japanese = {}
 GameText.LastUpdated.Japanese = "2015-08-26"
 GameText.HelperText.Japanese[ "I know Japanese" ]               = "にほんご　わかるよ。"
 GameText.HelperText.Japanese[ "I am learning English" ]         = "えいご　を　べんきょう　してるんだ。"
-GameText.HelperText.Japanese[ "I am learning Esperanto" ]       = "エスペラント　を　べんきょう　してるんだ。"
-GameText.HelperText.Japanese[ "I am learning Ido" ]             = "イド　を　べんきょう　してるんだ。"
+GameText.HelperText.Japanese[ "I am learning Esperanto" ]       = "エスペラントご　を　べんきょう　してるんだ。"
+GameText.HelperText.Japanese[ "I am learning Ido" ]             = "イドご　を　べんきょう　してるんだ。"
 GameText.HelperText.Japanese[ "I am learning Spanish" ]         = "スペインご　を　べんきょう　してるんだ。"
-GameText.HelperText.Japanese[ "I am learning Ithkuil" ]         = "イスクイル　を　べんきょう　してるんだ。"
-GameText.HelperText.Japanese[ "I am learning French" ]          = ""
-GameText.HelperText.Japanese[ "I am learning German" ]          = ""
-GameText.HelperText.Japanese[ "I am learning Polish" ]          = ""
+GameText.HelperText.Japanese[ "I am learning Ithkuil" ]         = "イスクイルご　を　べんきょう　してるんだ。"
+GameText.HelperText.Japanese[ "I am learning French" ]          = "フランスご　を　べんきょう　してるんだ。"
+GameText.HelperText.Japanese[ "I am learning German" ]          = "ドイツご　を　べんきょう　してるんだ。"
+GameText.HelperText.Japanese[ "I am learning Polish" ]          = "ポーランドご　を　べんきょう　してるんだ。"
     
 GameText.HelperText.Polish = {}
 GameText.TargetText.Polish = {}
 GameText.LastUpdated.Polish = "2015-08-24"
-GameText.HelperText.Polish[ "I know Polish" ]                   = "Znam polski."
+GameText.HelperText.Polish[ "I know Polish" ]                   = "Znam polski"
 GameText.HelperText.Polish[ "I am learning English" ]           = "Uczę się angielskiego"
-GameText.HelperText.Polish[ "I am learning English" ]           = "Uczę się angielskiego"
+GameText.HelperText.Polish[ "I am learning Esperanto" ]         = ""
 GameText.HelperText.Polish[ "I am learning Ido" ]               = "Uczę się ido"
-GameText.HelperText.Polish[ "I am learning Ido" ]               = "Uczę się ido"
+GameText.HelperText.Polish[ "I am learning Spanish" ]           = ""
 GameText.HelperText.Polish[ "I am learning Ithkuil" ]           = "Uczę się ithkuil"
 GameText.HelperText.Polish[ "I am learning German" ]            = "Uczę się niemieckiego"
 GameText.HelperText.Polish[ "I am learning French" ]            = "Uczę się francuskiego"
 GameText.HelperText.Polish[ "I am learning Polish" ]            = "Uczę się polskiego"
+    
+GameText.HelperText.Persian = {}
+GameText.TargetText.Persian = {}
+GameText.LastUpdated.Persian = "2015-09-01"
+GameText.HelperText.Persian[ "I know Persian" ]                  =  ".من فارسی بلدم"
+GameText.HelperText.Persian[ "I am learning English" ]           = "من دارم انگلیسی یاد می‌گیرم"
+GameText.HelperText.Persian[ "I am learning Esperanto" ]         =  "من دارم اسپرانتو یاد می‌گیرم"
+GameText.HelperText.Persian[ "I am learning Ido" ]               = "من دارم ایدو یاد می‌گیرم"
+GameText.HelperText.Persian[ "I am learning Spanish" ]           = "من دارم اسپانیایی یاد می‌گیرم"
+GameText.HelperText.Persian[ "I am learning Ithkuil" ]           =  "من دارم زبان ایثکوییل یاد می‌گیرم"
+GameText.HelperText.Persian[ "I am learning German" ]            = "من دارم آلمانی یاد می‌گیرم"
+GameText.HelperText.Persian[ "I am learning French" ]            = "من دارم فرانسوی یاد می‌گیرم"
+GameText.HelperText.Persian[ "I am learning Polish" ]            = "من دارم لهستانی یاد می‌گیرم"
     
 -- Conlangs
     
 GameText.HelperText.Esperanto = {}
 GameText.TargetText.Esperanto = {}
 GameText.LastUpdated.Esperanto = "2015-09-01"
-GameText.HelperText.Esperanto[ "I know Esperanto" ]             = "Mi scipovas paroli Esperanton."
+GameText.HelperText.Esperanto[ "I know Esperanto" ]             = "Mi scipovas paroli Esperanton"
 GameText.HelperText.Esperanto[ "I am learning English" ]        = "Mi lernas la anglan lingvon"
 GameText.HelperText.Esperanto[ "I am learning Esperanto" ]      = "Mi lernas Esperanton"
 GameText.HelperText.Esperanto[ "I am learning Ido" ]            = "Mi lernas Idon"
 GameText.HelperText.Esperanto[ "I am learning Spanish" ]        = "Mi lernas la hispanan lingvon"
 GameText.HelperText.Esperanto[ "I am learning Ithkuil" ]        = "Mi lernas Ifkuilon"
-GameText.HelperText.Esperanto[ "I am learning German" ]         = ""
-GameText.HelperText.Esperanto[ "I am learning French" ]         = ""
-GameText.HelperText.Esperanto[ "I am learning Polish" ]         = ""
+GameText.HelperText.Esperanto[ "I am learning German" ]         = "Mi lernas la germanan lingvon"
+GameText.HelperText.Esperanto[ "I am learning French" ]         = "Mi lernas la francan lingvon"
+GameText.HelperText.Esperanto[ "I am learning Polish" ]         = "Mi lernas la polan lingvon"
     
 GameText.HelperText.Ido = {}
 GameText.TargetText.Ido = {}
 GameText.LastUpdated.Ido = "2015-09-01"
-GameText.HelperText.Ido[ "I know Ido" ]                         = "Me savas Ido."
+GameText.HelperText.Ido[ "I know Ido" ]                         = "Me savas Ido"
 GameText.HelperText.Ido[ "I am learning English" ]              = "Me lernas la Angla linguo"
 GameText.HelperText.Ido[ "I am learning Esperanto" ]            = "Me lernas Esperanto"
 GameText.HelperText.Ido[ "I am learning Ido" ]                  = "Me lernas Ido"
 GameText.HelperText.Ido[ "I am learning Spanish" ]              = "Me lernas la Hispana linguo"
 GameText.HelperText.Ido[ "I am learning Ithkuil" ]              = "Me lernas Ifkuilo"
-GameText.HelperText.Ido[ "I am learning German" ]               = ""
-GameText.HelperText.Ido[ "I am learning French" ]               = ""
-GameText.HelperText.Ido[ "I am learning Polish" ]               = ""
+GameText.HelperText.Ido[ "I am learning German" ]               = "Me lernas la Franca linguo"
+GameText.HelperText.Ido[ "I am learning French" ]               = "Me lernas la Germana linguo"
+GameText.HelperText.Ido[ "I am learning Polish" ]               = "Me lernas la Polona linguo"
 
 GameText.HelperText.Ithkuil = {}
 GameText.TargetText.Ithkuil = {}
@@ -125,8 +143,8 @@ GameText.HelperText.Ithkuil[ "I am learning Polish" ]           = ""
 GameText.HelperText.Interlingua = {}
 GameText.TargetText.Interlingua = {}
 GameText.LastUpdated.Interlingua = "2015-08-31"
-GameText.HelperText.Interlingua[ "I know Interlingua" ]         = "Io comprende Interlingua."
-GameText.HelperText.Interlingua[ "I am learning English" ]      = "Io apprende Anglese."
+GameText.HelperText.Interlingua[ "I know Interlingua" ]         = "Io comprende Interlingua"
+GameText.HelperText.Interlingua[ "I am learning English" ]      = "Io apprende Anglese"
 GameText.HelperText.Interlingua[ "I am learning Esperanto" ]    = "Io apprende Esperanto"
 GameText.HelperText.Interlingua[ "I am learning Ido" ]          = "Io apprende Ido"
 GameText.HelperText.Interlingua[ "I am learning Spanish" ]      = "Io apprende Espaniol"
---
+++ Merri Language Fantasy/Merri/Language/Translations/language_fa-IR.lua
@@ -0,0 +1,159 @@
+-- Reference "language_en-US.lua" for dialogue lines
+function SetupFarsi()
+    local helper = {};      local target = {}
+    
+    -- Translations --
+    
+    -- CREDITS SCREEN
+    helper[ "Translators" ]             = "مترجم‌ها"                    -- Header text: "Translators" for Translator credits section
+    helper[ "language-name" ]           = "فارسی"                    -- Name of this language (match this language file)
+    helper[ "Translation Credits" ]     = "Ali H."                    -- Translators, append your name to the list.
+    
+    helper[ "website" ]                 = ":وبگاه"                    -- Label for homepage URL
+    target[ "website" ] = helper[ "website" ]                   -- Ignore this
+    
+    helper[ "facebook" ]                =  ":فیس‌بوک"                  -- Label for Facebook URL
+    target[ "facebook" ] = helper[ "facebook" ]                 -- Ignore this
+    
+    helper[ "twitter" ]                 = ":توییتر"                  -- Label for Twitter URL
+    target[ "twitter" ] = helper[ "twitter" ]                   -- Ignore this
+    
+    -- GAME TITLE
+    target[ "game title" ]              = "فانتزی فارسی"                    -- Translate as "Fantasy of ____" for your language. (e.g., Fantazio de Esperanto)
+    
+    -- DEMO TEXT (Temporary)
+    target[ "Thank you for trying out Merri" ]                  =".مری! ممنون از این‌که تلاش کردی"
+    target[ "This is still a work in progress" ]                =  ".این [بازی] هنوز در دست ساخت است"
+    target[ "There may be errors or bugs" ]                     =  ".ممکن است اشتباه یا باگ‌هایی وجود داشته باشند"
+    target[ "Report problems at EMAIL" ]                        = "Rachel@Moosader.com مشکلات را گزارش کنید به"
+    target[ "Want to keep up with this game's progress?" ]      =  "می‌خواهید از پیشرفت‌های این بازی باخبر باشید؟"
+    
+    -- Ignore this block
+    helper[ "Thank you for trying out Merri" ] = target[ "Thank you for trying out Merri" ]
+    helper[ "This is still a work in progress" ] = target[ "This is still a work in progress" ]
+    helper[ "There may be errors or bugs" ] = target[ "There may be errors or bugs" ]
+    helper[ "Report problems at EMAIL" ] = target[ "Report problems at EMAIL" ]
+    helper[ "Want to keep up with this game's progress?" ] = target[ "Want to keep up with this game's progress?" ]
+    -- End ignore
+        
+    -- MAIN MENU BUTTONS
+    target[ "play" ]                    = "!شروع"                   -- Button to begin the game
+    target[ "previous" ]                = "قبلی"                  -- Go backward one page (one menu)
+    target[ "next" ]                    = "ادامه"                   -- Go forward one page (one menu)
+    target[ "options" ]                 = "گزینه‌ها"                 -- Configure options in a game
+    target[ "help" ]                    = "کمک"                   -- Get help / instructions for a game or function
+    target[ "menu" ]                    =  "منو"               -- Open up sub-menu screen
+    target[ "close-menu" ]              =  "بازگشت"                  -- Close sub-menu
+    target[ "back" ] = target[ "close-menu" ]                   -- Ignore this
+    target[ "progress" ]                = ""                    -- Progress / Statistics menu
+    helper[ "progress" ] = target[ "progress" ]
+    
+    -- Difficulty levels
+    helper[ "Practice" ]  = "تمرین"
+    helper[ "Easy" ]      = "آسان"
+    helper[ "Medium" ]    = "میانی"
+    helper[ "Hard" ]      = "دشوار"
+    
+    -- Ignore this block
+    target[ "Practice" ] = helper[ "Practice" ]
+    target[ "Easy" ] = helper[ "Easy" ]
+    target[ "Medium" ] = helper[ "Medium" ]
+    target[ "Hard" ] = helper[ "Hard" ]
+    -- End ignore
+    
+    -- COMMON GAME TEXT
+    target[ "Earned a star" ]           =  "!یک ستاره گرفتید"
+    helper[ "Earned a star" ] = target[ "Earned a star" ]       -- Ignore this
+    
+    --------------------------
+    -- MINER MINI-GAME TEXT --
+    --------------------------
+    -- Stats
+    target[ "Miner" ]                   = "معدنچی"                    -- Male miner (temporary)
+    target[ "Floor" ]                   =":طبقه"                   -- How many floors down you have traveled in the mine
+    target[ "Money" ]                   = ":پول"                  -- The amount of (generic) money you've earned
+    target[ "Health" ]                  =  ":سلامتی"                   -- Amount of hits you have before you die
+    
+    -- Messages
+    target[ "Game Over" ]               =  "بازی تمام شد"
+    helper[ "Game Over" ] = target[ "Game Over" ]   -- Ignore this
+    
+    
+    -- Enemies
+    target[ "snail" ]                   = "حلزون"
+    target[ "mole" ]                    = "موش کور"
+    target[ "mushroom" ]                = "قارچ"
+    target[ "bat" ]                     = "خفاش"
+    target[ "rabbit" ]                  = "خرگوش"
+    target[ "snake" ]                   =  "مار"
+    target[ "skeleton" ]                ="اسکلت"
+    target[ "moose" ]                   = "گوزن"
+    
+    -- Items
+    target[ "rock" ]                    =  "سنگ"                    -- Large stone to break open with a pickaxe
+    target[ "star" ]                    = "ستاره"
+    target[ "sandwich" ]                = "ساندویچ"
+    target[ "amethyst" ]                =  "آمیتیست"
+    target[ "sapphire" ]                ="یاقوت کبود"
+    target[ "diamond" ]                 =  "الماس"
+    target[ "emerald" ]                 = "زمرد"
+    target[ "ruby" ]                    = "یاقوت سرخ"
+    target[ "copper" ]                  =  "مس"                    -- It is a copper coin
+    target[ "silver" ]                  = "نقره"
+    target[ "gold" ]                    = "طلا"
+    target[ "ladder" ]                  =  "نردبان"                  -- Ladder to climb down one floor
+    
+    -- Shopkeeper Dialogue
+    target[ "Do you want to buy a tool?" ]    =  "آیا می‌خواهید یک ابزار بخرید؟"
+    target[ "You don't have enough money" ]   = ".پول کافی ندارید"
+    
+    -- Button options
+    target[ "Don't buy anything" ]      =  ".هیچ چیز نخر"                   -- Click to leave the store, nobody is saying this line it is a button.
+    target[ "Price" ]                   =  "قیمت"                   -- Price label
+    
+    -- Item names and descriptions
+    target[ "Potion" ]                  = "معجون"                  -- Restores health
+    target[ "PotionDescription" ]       =  ".تمام سلامتی شما را بازمی‌گرداند"
+    target[ "Earthquake" ]              = "زلزله"
+    target[ "EarthquakeDescription" ]   =  ".تمام سنگ‌ها را می‌شکند"
+    target[ "Dynamite" ]                = "دینامیت"
+    target[ "DynamiteDescription" ]     =".یک طبقه پایین می‌روید"
+    target[ "Rope" ]                    = "طناب"
+    target[ "RopeDescription" ]         = ".یک طبقه بالا می‌روید"
+    target[ "Blizzard" ]                ="کولاک"
+    target[ "BlizzardDescription" ]     =  ".همه‌ی دشمن‌ها یخ می‌زنند"
+    
+    -- Narration
+    target[ "miner-begin" ]             = "!برای گنج حفر کن و زنده بمان"
+    target[ "miner-move-north" ]        = ".به سمت شمال راه می‌روید"
+    target[ "miner-move-south" ]        = ".به سمت جنوب راه می‌روید"
+    target[ "miner-move-east" ]         = ".به سمت شرق راه می‌روید"
+    target[ "miner-move-west" ]         = ".به سمت جنوب راه می‌روید"
+    target[ "miner-mine" ]              = ".با کلنگ ضربه می‌زنید"
+    target[ "miner-eat-sandwich" ]      = ".ساندویچ غاری را می‌خورید"
+    target[ "use-earthquake" ]          = ".زمین به شدت می‌لرزد"
+    target[ "use-blizzard" ]            = ".همه چیز یخ می‌زند"
+    target[ "enemies-thaw" ]            = ".همه چیز ذوب شده است"
+    target[ "use-potion" ]              = ".دوباره احساس قدرت می‌کنید"
+    target[ "miner-collect-cooper" ]    = ".مقداری مس برمی‌دارید"
+    target[ "miner-collect-silver" ]    = ".مقداری نقره برمی‌دارید"
+    target[ "miner-collect-gold" ]      = ".مقداری طلا برمی‌دارید"
+    target[ "miner-collect-amethyst" ]  = ".مقداری مس برمی‌دارید"
+    target[ "miner-collect-sapphire" ]  = ".مقداری یاقوت کبود برمی‌دارید"
+    target[ "miner-collect-diamond" ]   = ".مقداری الماس برمی‌دارید"
+    target[ "miner-collect-emerald" ]   = ".مقداری زمرد برمی‌دارید"
+    target[ "miner-collect-ruby" ]      = ".مقداری یاقوت سرخ برمی‌دارید"
+    target[ "miner-attack-snail" ]      = ".به حلزون حمله می‌کنید"
+    target[ "miner-attack-mole" ]       = ".به موش کور حمله می‌کنید"
+    target[ "miner-attack-mushroom" ]   = ".به قارچ حمله می‌کنید"
+    target[ "miner-attack-bat" ]        = ".به خفاش حمله می‌کنید"
+    target[ "miner-attack-rabbit" ]     = ".به خرگوش حمله می‌کنید"
+    target[ "miner-attack-snake" ]      = ".به مار حمله می‌کنید"
+    target[ "miner-attack-moose" ]      = ".به گوزن می‌کنید"
+    target[ "miner-attack-skeleton" ]   = ".به اسکلت حمله می‌کنید"
+    target[ "miner-go-down-ladder" ]    = ".از نردبان پایین می‌روید"
+    target[ "miner-rope" ]              = ".با طناب به سمت بالا فرار می‌کنید"
+    target[ "miner-dynamite" ]          = ".با دینامیت رو به پایین فرار می‌کنید"
+    target[ "miner-died" ]              = ".شما کشته شدید"
+    
+end
